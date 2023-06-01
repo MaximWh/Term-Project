8383c73df503ada9c37ef310e124e34bd3edfb82
MaximWh <108838433+MaximWh@users.noreply.github.com>
2023-06-01 04:07:44 -0400

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
 let flag=getCookie("flag");
 if(flag =="true") document.writeln("<td width='100' align=center><a href='logoff.html'><span class='button'>Logoff</a></td>");
-else document.writeln("<td width='100' align=center><a href='login.html'><span class='button'>Login</a></td>");	
+else document.writeln("<td width='100' align=center><a href='index.html'><span class='button'>Login</a></td>");	
 document.writeln("<td width='100' align=center><a href='about.html'><span class='button'>About</a></td>");  
 document.writeln("<td width='100' align=center><a href='products.html'><span class='button'>Product List</a></td>");	
 document.writeln("<td width='100' align=center><a href='contact.html'><span class='button'>Contact Us</a></td>");
